a1df0815602d7096567a6ee025d22cc1ed98755e
Daniel V Richardson <danielrichardson.v@shloklabs.com>
2024-01-21 02:18:14 +0530

    Finished Code

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -17,7 +17,7 @@ function Navbar() {
   const [open, setOpen] = useState(false);
 
   const user = JSON.parse(localStorage.getItem("user"));
-  console.log(user.user.email);
+
 
   const logout = () => {
     localStorage.clear("user");

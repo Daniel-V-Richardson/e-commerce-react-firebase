37d1a17849f5f76a21f47eb8f7fe236c6f0e9109
Daniel V Richardson <danielrichardson.v@shloklabs.com>
2024-01-21 09:16:19 +0530

    Finished Code

--- src/fireabase/FirebaseConfig.jsx
+++ src/fireabase/FirebaseConfig.jsx
@@ -3,26 +3,15 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDoJvalILWn6esetahpL9BGRT__B5azWvI",
-//   authDomain: "myfirstapp-38751.firebaseapp.com",
-//   projectId: "myfirstapp-38751",
-//   storageBucket: "myfirstapp-38751.appspot.com",
-//   messagingSenderId: "273202707457",
-//   appId: "1:273202707457:web:5724f212508b5b82f31400"
-// };
 const firebaseConfig = {
-  apiKey: "AIzaSyCjh78gNRnYJVKgUn0WJJK5gRf0RMPSbTI",
-  authDomain: "e-store-1995d.firebaseapp.com",
-  projectId: "e-store-1995d",
-  storageBucket: "e-store-1995d.appspot.com",
-  messagingSenderId: "683857057881",
-  appId: "1:683857057881:web:afdeb88cb16c39878b7948"
+  apiKey: "AIzaSyAR8Y5UAu1NGfaGHNYJ_GgcORjNG_YcTzg",
+  authDomain: "bharatstore-f562e.firebaseapp.com",
+  projectId: "bharatstore-f562e",
+  storageBucket: "bharatstore-f562e.appspot.com",
+  messagingSenderId: "910877265153",
+  appId: "1:910877265153:web:d45c1c14912ffef1e85f00",
 };
 
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 const fireDB = getFirestore(app);
